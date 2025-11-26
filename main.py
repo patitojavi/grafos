c1cdf8a5ba36e18cc663c7b09984db67675e6215
Patricio Benavides <114245124+patitojavi@users.noreply.github.com>
2025-11-26 20:37:34 -0300

    feat: agregar módulos iniciales (main e interfaz_controles)
    incluir módulos funcionales para interfaz y flujo principal del programa

---
+++ main.py
@@ -0,0 +1,12 @@
+from datos_viajante import obtener_ciudades, crear_instancia_viajante
+from interfaz_viajante import iniciar_interfaz
+
+def main() -> None:
+    total_ciudades = 9
+    ciudades = obtener_ciudades(total_ciudades)
+    instancia = crear_instancia_viajante(ciudades)
+
+    iniciar_interfaz(instancia)
+
+if __name__ == "__main__":
+    main()
