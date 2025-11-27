e9e13ae46bd63c63c482974ac577f840a1fa04ff
Joaco <canteroxx92@gmail.com>
2025-11-27 19:48:44 -0300

    fix: se dejo el main configurado para graficar con tkinter

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from datos_viajante import obtener_ciudades, crear_instancia_viajante
+from interfaz_viajante import iniciar_interfaz
 
 def main() -> None:
-    total_ciudades = 9 # maximo 11 ciudades debio al conjunto de datos
-    ciudades = obtener_ciudades(total_ciudades) # obtiene las ciudades
-    instancia = crear_instancia_viajante(ciudades) # crea la instancia del viajante
+    total_ciudades = 9 # maximo 11 ciudades o explota debito a la gran cantidad de rutas
+    ciudades = obtener_ciudades(total_ciudades) 
+    instancia = crear_instancia_viajante(ciudades)
+
+    iniciar_interfaz(instancia)
 
 if __name__ == "__main__":
     main()
